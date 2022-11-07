fc0e9b2fb459a89aca3b3688e3cc0bc882aca4ee
Subhadip Maji <subhadipmaji93@gmail.com>
2022-11-07 18:56:14 +0530

    add clear button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myButton = document.getElementsByClassName('myButton')[0];
+let clrButton = document.getElementsByClassName('clr')[0];
 myButton.addEventListener("click", function(){
     let value = document.getElementsByTagName('input')[0].value || document.cookie.split("=")[1];
     if(value){
@@ -7,15 +8,18 @@ myButton.addEventListener("click", function(){
       return;
     } 
     alert('nothing to show!!');
-  });
+});
 
+clrButton.addEventListener("click", function(){
+  document.cookie += document.cookie + '; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+});
 
-  function showText(value){
-    let showMessage = document.getElementsByTagName("p")[0];
-    showMessage.innerText = 'Hello ' + value  + '!! 🧡🧡';
-    showMessage.style.display = "block";
-    const animation = new fadeInAnimation(showMessage);
-    animation.start(1000);
+function showText(value){
+  let showMessage = document.getElementsByTagName("p")[0];
+  showMessage.innerText = 'Hello ' + value  + '!! 🧡🧡';
+  showMessage.style.display = "block";
+  const animation = new fadeInAnimation(showMessage);
+  animation.start(1000);
 }
 /*
 function fadeOut(elm){
@@ -70,3 +74,4 @@ function fadeInAnimation(node){
     this.duration = 0;
   }
 }
+
